22a90a243ce28bc1557e14dec0b5f094b7cc282f
Christopher Peret <cperet89@gmail.com>
2023-07-24 15:28:31 -0700

    layout style

--- src/components/portfolio/layout.js
+++ src/components/portfolio/layout.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import Style from './BaseLayout.module.scss'
+import Style from './assets/css/layout.module.scss'
 import Navbar from "./Navbar";
-import Home from "./home/Home";
-import About from "./about/About";
+import Home from "../home/home";
+import About from "../about/About";
 import Portfolio from "./portfolio/Portfolio";
 import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
