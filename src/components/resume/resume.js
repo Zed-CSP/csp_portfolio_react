3f027e535bb843fc5c2897a08d9f0b06d476df76
Christopher Peret <cperet89@gmail.com>
2023-07-27 12:14:02 -0700

    png routesfix

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Box, Grid} from "@mui/material";
-import cspresume from "../img/christopherPeretResumeDev.png"
-
+import resumeImg from '../img/christopherPeretResumeDev.png';
 // returns a box with a grid inside of it
 // the grid has two columns on mobile and two columns on desktop
 // the grid has a PortfolioBlock component in each column
@@ -11,9 +10,8 @@ export default function Resume() {
     return (
         <Box>
             <Grid container display={'flex'} justifyContent={'center'}>
-                <object data="../img/christopherPeretResumeDev.png" type="image" width="100%" height="500px">
-                    <p>Unable to displayfile. <a href="../img/christopherPeretResumeDev.png"
-                    >Download</a> instead.</p>
+                <object data={resumeImg} type="image/png" width="100%" height="500px">
+                    <p>Unable to display file. <a href={resumeImg}>Download</a> instead.</p>
                 </object>
             </Grid>
         </Box>
